dcd2b0479b3a430c554712af7fb75531f355607e
n1EzeR <erasylz@gmail.com>
2019-12-14 00:04:16 +0600

    add products and reviews crawler via api

---
+++ code/products_crawler.py
@@ -0,0 +1,28 @@
+from scrapy.crawler import CrawlerProcess
+from spiders import list
+
+
+def start():
+    process = CrawlerProcess()
+    # process.crawl(list.SmartphonesSpider)
+    # process.crawl(list.PerfumeSpider)
+    # process.crawl(list.BeautySpider)
+    # process.crawl(list.BooksSpider)
+    # process.crawl(list.CarAudioSpider)
+    # process.crawl(list.CarElectronicsSpider)
+    # process.crawl(list.HeadphonesSpider)
+    # process.crawl(list.WatchesSpider)
+    # process.crawl(list.WearablesSpider)
+    # process.crawl(list.BigHomeAppliancesSpider)
+    # process.crawl(list.SmallHomeAppliancesSpider)
+    # process.crawl(list.ClimateEquipmentSpider)
+    # process.crawl(list.KitchenAppliancesSpider)
+    # process.crawl(list.MemoryCardsSpider)
+    # process.crawl(list.PortableSpeakersSpider)
+    # process.crawl(list.PowerBanksSpider)
+    # process.crawl(list.TiresSpider)
+    process.start()
+
+
+if __name__ == '__main__':
+    start()
---
+++ code/reviews_crawler.py
@@ -0,0 +1,28 @@
+from scrapy.crawler import CrawlerProcess
+from spiders import reviews
+
+
+def start():
+    process = CrawlerProcess()
+    # process.crawl(reviews.SmartphonesSpider)
+    # process.crawl(reviews.PerfumesSpider)
+    # process.crawl(reviews.BeautySpider)
+    # process.crawl(reviews.BooksSpider)
+    # process.crawl(reviews.CarAudioSpider)
+    # process.crawl(reviews.CarElectronicsSpider)
+    # process.crawl(reviews.HeadphonesSpider)
+    # process.crawl(reviews.WatchesSpider)
+    # process.crawl(reviews.WearablesSpider)
+    # process.crawl(reviews.BigHomeAppSpider)
+    # process.crawl(reviews.SmallHomeAppSpider)
+    # process.crawl(reviews.ClimateEquipmentSpider)
+    # process.crawl(reviews.KitchenHomeAppSpider)
+    # process.crawl(reviews.MemoryCardsSpider)
+    # process.crawl(reviews.PortableSpeakersSpider)
+    # process.crawl(reviews.PowerBanksSpider)
+    # process.crawl(reviews.TiresSpider)
+    process.start()
+
+
+if __name__ == '__main__':
+    start()
