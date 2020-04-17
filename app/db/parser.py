72baf919f93ac4a861d55dd8d79a4279094587dc
n1EzeR <erasylz@gmail.com>
2020-04-17 20:27:57 +0600

    Process specs before dumping into db

--- app/db/parser.py
+++ app/db/parser.py
@@ -8,12 +8,11 @@ import orjson
 from app import utils
 from app.constants import DB_DUMPS_DIR, PRODUCTS_DIR, REVIEWS_DIR, SPECS_DIR
 from app.db import schemas
-from app.db.session import LocalSession
+from app.db.processing import processed_specs
 from app.utils import perf_logger
 
 PRODUCTS_DIR = f"../{PRODUCTS_DIR}"
 REVIEWS_DIR = f"../{REVIEWS_DIR}"
-SPECS_DIR = f"../{SPECS_DIR}/all"
 
 DB_DUMPS_DIR = f"../{DB_DUMPS_DIR}/{date.today()}"
 os.makedirs(DB_DUMPS_DIR, exist_ok=True)
@@ -103,58 +102,16 @@ def parse_reviews():
 
 @perf_logger
 def parse_specs():
-    products = os.listdir(SPECS_DIR)
-    products = [p for p in products if not p.startswith(".")]
-    dumped_products = _get_dumped_products()
+    with open(f"{SPECS_DIR}/computers-specs.json") as f:
+        products = json.load(f)
 
     db_specs = []
     for product in products:
-        with open(f"{SPECS_DIR}/{product}") as f:
-            product = json.load(f)
-            if int(product["id"]) not in dumped_products:
-                continue
+        specs = processed_specs(product)
+        db_specs.append(specs)
 
-            specs = _parse_product_specs(product)
-            db_specs.append(specs)
-
-    with open(f"{DB_DUMPS_DIR}/specs.json", "w") as f:
+    with open(f"{DB_DUMPS_DIR}/computers-specs.json", "w") as f:
         json.dump(db_specs, f, ensure_ascii=False)
-
-
-def _parse_product_specs(product):
-    details = _cleaned_dict(product["parsed_details"])
-    return {
-        "product_id": product["id"],
-        "type": details.get("Тип"),
-        "cpu": details.get("Процессор"),
-        "hertz": details.get("Частота процессора", details.get("Производительность")),
-        "cores": details.get(
-            "Количество ядер процессора", details.get("Производительность")
-        ),
-        "gpu": details.get("Видеопроцессор", details.get("Видеокарта")),
-        "ram": details.get(
-            "Размер оперативной памяти", details.get("Оперативная память")
-        ),
-        "ssd": details.get(
-            "Твердотельный диск (SSD)", details.get("Тип жесткого диска")
-        ),
-        "drive_size": details.get(
-            "Объем жесткого диска", details.get("Общий объем накопителей")
-        ),
-        "camera": details.get("Фотокамера"),
-        "battery": details.get("Аккумулятор"),
-        "extra": json.dumps(details, ensure_ascii=False),
-    }
-
-
-def _cleaned_dict(d: dict):
-    return {k: v.strip().replace("&nbsp", " ") for k, v in d.items()}
-
-
-def _get_dumped_products():
-    db_session = LocalSession()
-    fetch_all = db_session.select_all("SELECT source_id FROM product")
-    return {x[0] for x in fetch_all}
 
 
 if __name__ == "__main__":
---
+++ app/db/processing.py
@@ -0,0 +1,204 @@
+import json
+import re
+
+from app.utils import perf_logger
+
+
+@perf_logger
+def processed_specs(product):
+    return _parse_product_specs(product)
+
+
+def _parse_product_specs(product):
+    details = _cleaned_dict(product["parsed_details"])
+
+    gpu = details.get("Видеопроцессор", details.get("Видеокарта"))
+    gpu = _cleaned_gpu(gpu)
+
+    hertz = details.get("Частота процессора", details.get("Производительность"))
+    hertz = _extract_hertz(hertz)
+
+    cores = details.get("Количество ядер процессора", details.get("Производительность"))
+    cores = _extract_cores(cores)
+
+    ram = details.get("Размер оперативной памяти", details.get("Оперативная память"))
+    ram_size = _extract_ram_size(ram)
+    ram_type = _extract_ram_type(ram)
+
+    ssd = details.get("Твердотельный диск (SSD)", details.get("Тип жесткого диска"))
+    ssd = _extract_ssd(ssd)
+
+    drive_size = details.get(
+        "Объем жесткого диска", details.get("Общий объем накопителей")
+    )
+    drive_size = _extract_drive_size(drive_size)
+
+    return {
+        "product_id": product["id"],
+        "type": details.get("Тип"),
+        "cpu": details.get("Процессор"),
+        "hertz": hertz,
+        "cores": cores,
+        "gpu": gpu,
+        "ram": ram_size,
+        "ram_type": ram_type,
+        "ssd": ssd,
+        "drive_size": drive_size,
+        "camera": details.get("Фотокамера"),
+        "battery": details.get("Аккумулятор"),
+        "extra": json.dumps(details, ensure_ascii=False),
+    }
+
+
+def _cleaned_dict(d: dict):
+    return {k: v.strip().replace("&nbsp", " ") for k, v in d.items()}
+
+
+def _cleaned_gpu(gpu: str):
+    """
+    Clean GPU
+
+    GPU:
+        if any russian letter in word, drop the word
+            - but if ends with "," then add "+"
+        if only english letters in word and ends with ","
+            - then add "+"
+            - then add word
+    """
+    if not gpu:
+        return
+
+    words = gpu.split()
+    result = []
+    for word in words:
+        if word == "и":
+            result.append("+")
+            continue
+
+        if "в" in word or "д" in word:
+            if word.endswith(","):
+                result.append("+")
+            continue
+
+        if word.endswith(","):
+            result.append(word[:-1])
+            result.append("+")
+            continue
+
+        result.append(word)
+
+    return " ".join(result).strip("+").strip()
+
+
+def _extract_cores(raw_cores: str):
+    """
+    Extract cores number
+
+    raw_cores:
+        - 2-х ядерный 2.4 ГГц
+        - 4-х ядерный, 1.6 ГГц
+        - 1600 МГц, 4 ядра
+        - 2000 MHz, 4 ядра
+        - 8 ядер
+        - 4 ядра
+    """
+    if not raw_cores:
+        return
+
+    patterns = (
+        r"(\d+)-х ядерный",
+        r"(\d) ядра",
+        r"(\d) ядер",
+    )
+    for pattern in patterns:
+        match = re.search(pattern, raw_cores)
+        if match:
+            return match.group(1)
+
+
+def _extract_hertz(raw_hertz: str):
+    """
+    Extract hertz
+
+    Hertz:
+        - 2-х ядерный 2.4 ГГц
+        - 4-х ядерный, 1.6 ГГц
+        - 1600 МГц, 4 ядра
+        - 2000 MHz, 4 ядра
+        - 8 ядер
+        - 4 ядра
+        - 2 ядра, 2300 МГц
+        - 3.9 МГц
+        - 1,6 ГГц - ускорение Turbo Boost до 3,9 ГГц
+    """
+    if not raw_hertz:
+        return
+
+    patterns_mhz = (r"(\d\d\d\d)",)
+    for pattern in patterns_mhz:
+        match = re.search(pattern, raw_hertz)
+        if match:
+            return match.group()
+
+    patterns_ghz = (
+        r"(\d\.\d)",
+        r"(\d,\d)",
+    )
+    for pattern in patterns_ghz:
+        match = re.search(pattern, raw_hertz)
+        if match:
+            hertz = float(match.group().replace(",", ".")) * 1000
+            return int(hertz)
+
+
+def _extract_ram_size(raw_ram: str):
+    """
+    Extract RAM size
+
+    raw_ram:
+        - 8 ГБ, 8 Гб, 8Гб
+        - 8 GB, 8 Gb, 8Gb
+    """
+    if not raw_ram:
+        return
+
+    patterns_size = (
+        r"(\d)\s*[Гг][Бб]",
+        r"(\d)\s*[Gg][Bb]",
+    )
+
+    for pattern in patterns_size:
+        match = re.search(pattern, raw_ram)
+        if match:
+            return match.group(1)
+
+
+def _extract_ram_type(ram: str):
+    """Extract RAM type (DDR3, DDR4, etc.)"""
+    if not ram:
+        return
+
+    pattern = r"L?P?DDR\d[Lx]?"
+    match = re.search(pattern, ram)
+    if match:
+        return match.group()
+
+
+def _extract_ssd(ssd: str):
+    return ssd and (ssd == "да" or "SSD" in ssd)
+
+
+def _extract_drive_size(drive_size: str):
+    """
+    Extract Drive size
+
+    drive_size:
+        - 1024 Гб
+    """
+    if not drive_size:
+        return
+
+    pattern = r"(\d+) \w{2}"
+    match = re.search(pattern, drive_size)
+    if match:
+        return match.group(1)
